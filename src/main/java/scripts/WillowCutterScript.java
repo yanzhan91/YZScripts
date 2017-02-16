dd8bcb57cc194110f693fc965173747d435e3d7b
Yan Zhan <yanzhan91@gmail.com>
2017-02-16 00:33:17 -0600

    Add antiban and error check for willow cutter

--- src/main/java/scripts/WillowCutterScript.java
+++ src/main/java/scripts/WillowCutterScript.java
@@ -1,14 +1,17 @@
 package scripts;
 
-import org.tribot.api2007.Inventory;
+import org.tribot.api.util.abc.ABCUtil;
+import org.tribot.api2007.*;
 import org.tribot.api2007.Objects;
-import org.tribot.api2007.Player;
-import org.tribot.api2007.Walking;
 import org.tribot.api2007.ext.Filters;
+import org.tribot.api2007.types.RSItem;
 import org.tribot.api2007.types.RSObject;
 import org.tribot.api2007.types.RSTile;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 @ScriptManifest(authors = {"YZ"}, category = "WoodCutting", name = "YZWillowCutterScript")
@@ -19,25 +22,40 @@ public class WillowCutterScript extends Script {
     private static final int WILLOW1 = 1750;
     private static final int WILLOW2 = 1756;
 
+    private Random random = new Random((new Date()).getTime());
+    private ABCUtil abcUtil = new ABCUtil();
+    private HashSet<Integer> hatchets = Arrays.stream(Constants.IDs.Items.hatchets).boxed().collect(Collectors.toCollection(HashSet::new));
+
     @Override
     public void run() {
         try {
             while (true) {
+                sleep(3000);
+                checkForAxe();
                 movePlayerIntoPosition();
                 checkInventoryAndDrop();
                 if (Player.getAnimation() != -1) {
+                    performAntiBan();
                     continue;
                 }
                 findWillowAndCut();
-                sleep(3000);
             }
         } catch (YZScriptException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    private void checkForAxe() {
+        boolean axeEquipped = Equipment.getItem(Equipment.SLOTS.WEAPON).name != null && Equipment.getItem(Equipment.SLOTS.WEAPON).name.contains("axe");
+        boolean axeInInventory = Arrays.stream(Inventory.getAll()).anyMatch(item -> hatchets.contains(item.getID()));
+        if (!(axeEquipped || axeInInventory)) {
+            throw new YZScriptException("No axe found");
+        }
+    }
+
     private void findWillowAndCut() {
-        RSObject[] willowTrees = Objects.findNearest(1, Filters.Objects.idEquals(WILLOW1, WILLOW2));
+        RSObject[] willowTrees = Objects.findNearest(2, Filters.Objects.idEquals(WILLOW1, WILLOW2));
+        System.out.println("Number of trees " + willowTrees.length);
         if (willowTrees.length > 0) {
             willowTrees[0].click();
         } else {
@@ -60,6 +78,27 @@ public class WillowCutterScript extends Script {
         while (Player.getPosition().distanceTo(POSITION) > 1) {
             Walking.walkTo(POSITION);
             sleep(1000);
+        }
+    }
+
+    private void performAntiBan() {
+        switch(this.random.nextInt(30)) {
+            case 1:
+                this.abcUtil.moveMouse();
+                break;
+            case 2:
+                this.abcUtil.checkTabs();
+                this.sleep(2000L);
+                break;
+            case 3:
+                this.abcUtil.checkXP();
+                this.sleep(2000L);
+                break;
+            case 4:
+                this.abcUtil.pickupMouse();
+                break;
+            case 5:
+                this.abcUtil.rightClick();
         }
     }
 }
